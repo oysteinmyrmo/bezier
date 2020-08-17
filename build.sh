11181c870253c96535af516b58c652c712c1de92
Øystein Myrmo <oystein@myvr-software.com>
2020-08-17 21:48:35 +0200

    Add --output-on-failure option to ctest

--- build.sh
+++ build.sh
@@ -11,9 +11,9 @@ cd build
 if [[ "$OS" == "Windows_NT" ]]; then
     cmake ..
     cmake --build . --config Release
-    ctest -C Release --verbose .
+    ctest -C Release --verbose --output-on-failure .
 else
     cmake -DCMAKE_BUILD_TYPE=Release ..
     cmake --build .
-    ctest --verbose .
+    ctest --verbose --output-on-failure .
 fi
