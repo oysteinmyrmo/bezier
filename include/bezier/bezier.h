6771e252dbecd519f6b0a8ea4c7b2b4e7534137c
Øystein Myrmo <oystein.myrmo@gmail.com>
2023-08-13 12:30:54 +0200

    Update copyright year

--- include/bezier/bezier.h
+++ include/bezier/bezier.h
@@ -1,4 +1,4 @@
-/* Copyright © 2018 Øystein Myrmo (oystein.myrmo@gmail.com)
+/* Copyright © 2023 Øystein Myrmo (oystein.myrmo@gmail.com)
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
--- tests/derivative-tests.cpp
+++ tests/derivative-tests.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright © 2017 Oystein Myrmo
+ * Copyright © 2023 Oystein Myrmo
  *
  * Permission to use, copy, modify, distribute, and sell this software and its
  * documentation for any purpose is hereby granted without fee, provided that
